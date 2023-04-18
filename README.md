1e8bb20788ce218eeadb0dc678c181b7d4cf799c
Pratikpharpat <131173162+Pratikpharpat@users.noreply.github.com>
2023-04-18 20:09:25 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-# Pratik_Basic-Banking-System
 # the-sparks-foundation
 Sparks Foundation Internship Project : Basic Banking System  
 A Web Application used to transfer money between multiple users (Project contains 10 dummy users). A dummy user can also be created.  
@@ -10,6 +9,17 @@ Database : MySQL
 
 Database contains two Tables- Users Table & Transaction Table 
 1. User table have basic fields such as name, email & current balance. 
-2. Transaction table records all transfers happened along with their time.  
+2. Transaction table records all transfers happened along with their time.
+
+   HOW TO START :-
+1. Clone my repository or download the zip file
+2. If you're downloading the zip file, extract the zip file to a folder
+3. Open the folder with a code editor like VS Code or any IDE.
+4. Create two tables in a database - One to store user details and one to store transaction details.
+5. Create the necessery columns.
+6. Update my Config.php file with your database details.
+7. Run the code with XAAMP. You can reach out to me for queries. Details in my profile readme.
+
 
 Flow of the Website: Home Page > View all Users > Select and View one User > Transfer Money > Select reciever > View all Users > View Transfer History.
+
---
+++ navbar.php
@@ -0,0 +1,30 @@
+<!-- navbar --> 
+      <nav class="navbar navbar-expand-md navbar-light bg-info sticky-top" >
+      <a class="navbar-brand" href="index.php">SCS BANK</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="index.php">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="createuser.php">Create User</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transfermoney.php">Transfer Money</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transactionhistory.php">Transaction History</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="user.php">view user</a>
+              </li>
+          </div>
+       </nav>
+       <footer class="text-center mt-5 py-2">
+        <p>&copy 2023. Made with &hearts; by <b>Pratik Pharpat</b> <br>Web Development Intern @The Sparks Foundation</p>
+      </footer>
+      <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
